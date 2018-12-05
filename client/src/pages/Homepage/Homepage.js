cdeaf94c4b9d5328ec7650e4ea58172d82f20e14
Yuvashk <ashk0006@umn.edu>
2018-12-05 14:13:54 -0600

    removed modal

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Row, Container } from "../../components/Grid";
-import Modal from "../../components/Modal";
-import ModalTrigger from "../../components/ModalTrigger";
+import { Container } from "../../components/Grid";
 import './Homepage.css'
 
 class Homepage extends Component {
@@ -11,12 +9,6 @@ class Homepage extends Component {
         return (
 
             <Container fluid>
-                <Row>
-                    <Modal title="Sign In" modalID="signup" />
-                    <ModalTrigger IDof="#signup" buttonName="Sign In">
-                        <button>thisisabutton</button>
-                    </ModalTrigger>
-                </Row>
             </Container>
         )
     }

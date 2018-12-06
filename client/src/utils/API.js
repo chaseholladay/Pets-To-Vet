2fcc326aa34fdb038e960c7bec2d96191c2a1fe6
Yuvashk <ashk0006@umn.edu>
2018-12-06 11:46:15 -0600

    removed yarn.lock for heroku

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,27 +2,27 @@ import axios from "axios";
 
 export default {
   // Gets all Pets
-  getPets: function() {
+  getPets: function () {
     return axios.get("/api/Pets");
   },
   // Gets the Pet with the given id
-  getPet: function(id) {
+  getPet: function (id) {
     return axios.get("/api/Pets/" + id);
   },
   //gets a single user's data
-  getUser: function(id){
+  getUser: function (id) {
     return axios.get("/api/users" + id);
   },
   // Deletes the pet with the given id
-  deletePet: function(id) {
+  deletePet: function (id) {
     return axios.delete("/api/Pets/" + id);
   },
   // Saves a pet to the database
-  addPet: function(petData) {
+  addPet: function (petData) {
     return axios.post("/api/Pets", petData);
   },
   // Saves pet notes to the database
-  addPet: function(petNotes) {
+  addNote: function (petNotes) {
     return axios.post("/api/Notes", petNotes);
   }
 };

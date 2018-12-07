e6a7f2dc29407067250d9c37ed34c522792b3262
Yuvashk <ashk0006@umn.edu>
2018-12-06 22:49:04 -0600

    comment

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,7 +6,6 @@ const Nav = () => (
     <div className="nav-wrapper blue lighten-2">
       <a href="/" className="brand-logo">Pets to Vet</a>
       <ul className="right">
-        {/* <li> */}
         {Auth.isUserAuthenticated() ? (
           <div className="top-bar-right">
             <li><a href="/logout">Log out</a></li>
@@ -17,7 +16,6 @@ const Nav = () => (
               <li><a href="/signup">Sign up</a></li>
             </div>
           )}
-        {/* </li> */}
       </ul>
     </div>
   </nav>
